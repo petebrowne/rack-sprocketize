a7542e268f6b43fa591a6eeb53e2828e277633d3
Pete Browne <me@petebrowne.com>
2011-03-17 11:46:09 -0500

    Refactoring to use the Config class & use different configurations perinstance

--- lib/rack/sprocketize.rb
+++ lib/rack/sprocketize.rb
@@ -2,63 +2,16 @@ require 'rack'
 
 module Rack
   class Sprocketize
+    autoload :Config,   'rack/sprocketize/config'
+    autoload :Railtie,  'rack/sprocketize/railtie'
     autoload :Sprocket, 'rack/sprocketize/sprocket'
     autoload :VERSION,  'rack/sprocketize/version'
     
-    DEFAULT_OPTIONS = {
-      :source_path => 'app/javascripts',
-      :output_path => 'public/javascripts',
-      :load_path   => %w(vendor/javascripts)
-    }.freeze
-    
-    class << self
-      attr_accessor :options, :compression_options, :source_path, :output_path, :always_check, :always_compress
-      
-      def configure(options = {})
-        self.options             = DEFAULT_OPTIONS.dup.merge(options)
-        self.compression_options = self.options.delete(:compression_options) || {}
-        self.source_path         = ::File.expand_path self.options.delete(:source_path)
-        self.output_path         = ::File.expand_path self.options.delete(:output_path)
-      
-        self.always_check = if self.options.key?(:always_check)
-          self.options.delete(:always_check)
-        else
-          self.environment == 'development'
-        end
-      
-        self.always_compress = if self.options.key?(:always_compress)
-          self.options.delete(:always_compress)
-        else
-          self.environment == 'production'
-        end
-      end
-      
-      def environment
-        if defined?(RAILS_ENV)
-          RAILS_ENV # Rails 2
-        elsif defined?(Rails) && defined?(Rails.env)
-          Rails.env.to_s # Rails 3
-        elsif defined?(@app.settings) && defined?(@app.settings.environment)
-          @app.settings.environment # Sinatra
-        elsif ENV.key?('RACK_ENV')
-          ENV['RACK_ENV']
-        else
-          'development'
-        end
-      end
-      
-      def always_check?
-        !!@always_check
-      end
-      
-      def always_compress?
-        !!@always_compress
-      end
-    end
+    attr_accessor :config
     
     def initialize(app, options = {})
-      @app = app
-      Sprocketize.configure(options)
+      @app    = app
+      @config = Config.new(options)
     end
     
     def call(env)
@@ -70,19 +23,19 @@ module Rack
     protected
     
       def skip?
-        return false if Sprocketize.always_check?
+        return false if config.always_check?
         @sprocketized && @request.params['sprocketize'].nil?
       end
       
       def source_files
-        files = Dir.glob ::File.join(Sprocketize.source_path, '**/*.js')
+        files = Dir.glob ::File.join(config.source_path, '**/*.js')
         files.reject! { |file| ::File.basename(file) =~ /^_/ }
         files
       end
     
       def sprocketize
         source_files.each do |source_file|
-          sprocket = Sprocket.new(source_file)
+          sprocket = Sprocket.new(source_file, config)
           sprocket.sprocketize if sprocket.stale?
         end
         @sprocketized = true
--- lib/rack/sprocketize/sprocket.rb
+++ lib/rack/sprocketize/sprocket.rb
@@ -4,10 +4,13 @@ require 'sprockets'
 module Rack
   class Sprocketize
     class Sprocket
-      def initialize(source_file)
+      attr_accessor :source_file, :output_path, :config, :secretary
+      
+      def initialize(source_file, config)
         @source_file = source_file
-        @output_path = @source_file.sub /^#{Regexp.escape(Sprocketize.source_path)}/, Sprocketize.output_path
-        @secretary   = Sprockets::Secretary.new Sprocketize.options.merge(:source_files => [ @source_file ])
+        @config      = config
+        @output_path = source_file.sub /^#{Regexp.escape(config.source_path)}/, config.output_path
+        @secretary   = Sprockets::Secretary.new config.sprockets.merge(:source_files => [ @source_file ])
       end
       
       def compress(output)
@@ -25,18 +28,18 @@ module Rack
       end
       
       def concat
-        @secretary.concatenation.to_s
+        secretary.concatenation.to_s
       end
       
       def stale?
-        !::File.exist?(@output_path) || @secretary.source_last_modified > ::File.mtime(@output_path)
+        !::File.exist?(output_path) || secretary.source_last_modified > ::File.mtime(output_path)
       end
       
       def sprocketize
-        FileUtils.mkdir_p ::File.dirname(@output_path)
-        ::File.open(@output_path, 'w') do |file|
+        FileUtils.mkdir_p ::File.dirname(output_path)
+        ::File.open(output_path, 'w') do |file|
           output = concat
-          output = compress(output) if Sprocketize.always_compress? && !already_compressed?
+          output = compress(output) if config.always_compress? && !already_compressed?
           file.write(output.strip)
         end
       end
@@ -48,7 +51,7 @@ module Rack
       protected
       
         def compression_options(defaults = {})
-          defaults.merge Sprocketize.compression_options
+          defaults.merge(config.compression)
         end
     end
   end
